37563f653665ac9f748048139faeced44db65d8e
MouridZakaria <127679395+MouridZakaria@users.noreply.github.com>
2023-09-10 21:07:45 +0100

    Commit done

---
+++ calculator_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:calculator_app/CalculatorView.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
